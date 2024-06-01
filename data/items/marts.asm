6ee92b9b46a11e179b7ed00d82c898068ce0a2cf
Readmit <r685frr@gmail.com>
2024-05-31 23:31:49 -0700

    Commented out unused text

--- data/items/marts.asm
+++ data/items/marts.asm
@@ -1,5 +1,5 @@
 ; mart inventories are below
 ; they are texts
 
-UnusedMartClerkText:: ; unreferenced
-	script_mart GREAT_BALL, HYPER_POTION, SUPER_POTION, FULL_HEAL, REVIVE
+;UnusedMartClerkText:: ; unreferenced
+;	script_mart GREAT_BALL, HYPER_POTION, SUPER_POTION, FULL_HEAL, REVIVE
